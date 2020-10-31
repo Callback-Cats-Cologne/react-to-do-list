7fec3815e11f309eb126a4dd0f5aa99ff918f491
Matt-1985 <blatsche@web.de>
2020-10-31 13:24:45 +0100

    Add hover effect

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -4,9 +4,7 @@ export default function ToDoList({ todos }) {
   const taskElements = todos.map((task) => (
     <li key={task} tasks={task}>
       <input type="checkbox" />
-      <span contentEditable="true" className="editable">
-        {task}
-      </span>
+      <span>{task}</span>
       <button className="delete__btn">💥</button>
     </li>
   ));
